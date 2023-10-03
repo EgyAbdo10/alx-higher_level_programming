025d92495007880159933054d43de22be652a6cd
EgyAbdo10 <egyabdo490@gmail.com>
2023-10-03 13:58:01 -0700

    edit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if (i < (int(i / 10) / 10 + (i % 10) * 10)):
+    if (int(i % 10) > int(i / 10)):
         if (i != 89):
             print("{}{}".format(int(i / 10), (i % 10)), end=", ")
         else:
