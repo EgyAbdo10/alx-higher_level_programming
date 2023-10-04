d68c1084857eeae4511401a6747d3d245bf59616
EgyAbdo10 <egyabdo490@gmail.com>
2023-10-03 17:28:36 -0700

    edit

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+ * add_node_beg - add a node to the beginning of a list
+ * @head: a pointer to the head pointer
+ * @number: teh number to be in the node
+ * Return: a pointer to thh newly created node
+ */
+listint_t *add_node_beg(listint_t **head, int number)
+{
+listint_t *ptr;
+ptr = malloc(sizeof(listint_t));
+if (ptr == NULL)
+return (NULL);
+ptr->n = number;
+ptr->next = *head;
+*head = ptr;
+return (ptr);
+}
+/**
  * insert_node - insert a node in its right order
  * @head: a ponter to the head pointer
  * @number: the number to be put in the node
@@ -8,16 +25,17 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 listint_t *ptr, *new = *head;
-if (new->n > number || new == NULL)
+if (new == NULL)
 {
 ptr = malloc(sizeof(listint_t));
 if (ptr == NULL)
 return (NULL);
 ptr->n = number;
 ptr->next = new;
-new = ptr;
-return (new);
+return (ptr);
 }
+if (new->n > number)
+retrun (add_node_beg(head, number));
 while (new->next != NULL)
 {
 if (new->next->n < number)
