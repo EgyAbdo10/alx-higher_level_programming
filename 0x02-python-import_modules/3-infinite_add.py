759ba0ebbe4e7166deb01febe106deee4ef98d5a
EgyAbdo10 <egyabdo490@gmail.com>
2023-10-17 04:36:59 +0000

    added or modified a file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     sum = 0
-    for i ,v in enumerate(sys.argv):
+    for i, v in enumerate(sys.argv):
         if i > 0:
             sum += int(v)
     print("{}".format(sum))
