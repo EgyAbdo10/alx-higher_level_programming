0bbab1d3ba488577a73650712a1488db81d7657d
EgyAbdo10 <egyabdo490@gmail.com>
2023-10-17 04:26:50 +0000

    added or modified a file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("{} arguments.".format(0))
     elif len(sys.argv) == 2:
-        print("{}: argument".format(1))
+        print("{} argument:".format(1))
         print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{}: arguments".format(len(sys.argv)))
+        print("{}: arguments".format(len(sys.argv) - 1))
         for i, v in enumerate(sys.argv):
             if (i > 0):
-                print("{}: {}".format(i, v))
+                print("{}: {}".format(i, v)
