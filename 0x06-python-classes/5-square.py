025036f11d6a460076e40f15b6d813a5b389913c
EgyAbdo10 <egyabdo490@gmail.com>
2023-10-29 12:14:59 -0700

    added or modified a file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+this module defines a class square
+which is empty
+"""
+
+
+class Square:
+    """
+    this class is empty and just defines a square
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        this function calculates the area of a square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, new_size):
+        if type(new_size) != int:
+            raise TypeError("size must be an integer")
+        elif new_size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = new_size
+
+    def my_print(self):
+        """
+        this function prints a square in # format
+        """
+        i = 0
+        while i < self.__size:
+            print("#" * self.__size)
+            i += 1
+        if self.__size == 0:
+            print()
