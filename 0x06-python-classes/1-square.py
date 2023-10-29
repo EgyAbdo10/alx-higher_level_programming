ea0bd51e6daddbb1f1b503e64d0a60e66fea599b
EgyAbdo10 <egyabdo490@gmail.com>
2023-10-29 11:46:21 -0700

    added or modified a file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this module defines a class square
+which is empty
+"""
+
+
+class Square:
+    """
+    this class is empty and just defines a square
+    """
+    def __init__(self, size):
+        self.__size = size
