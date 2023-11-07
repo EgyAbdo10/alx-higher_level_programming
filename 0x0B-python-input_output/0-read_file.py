cabed7397c10d90801a4ec53093d86ab1cd5ede8
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-07 12:14:58 -0800

    added or modified a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""this module has a function that reads text files"""
+
+
 def read_file(filename=""):
+    """read text files from the beginning to the end"""
     with open(filename, encoding="UTF8") as file:
         print(file.read(), end="")
