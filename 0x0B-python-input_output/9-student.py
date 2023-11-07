6accc9a2ea7e55a8c711f1373e7d40357b3b0132
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-07 15:49:37 -0800

    added or modified a file

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
 
     def to_json(self):
         """get a dict from object attributes"""
