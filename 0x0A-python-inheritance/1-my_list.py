f343c914456b3fafe20a38afc36a6db094ccf42c
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-08 06:22:23 -0800

    added or modified a file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,7 @@
 class MyList(list):
     """this class inherits from list"""
     def print_sorted(self):
+        """print a list sorted without affecting the list"""
         sorted_list = self[:]
         sorted_list.sort()
         print(sorted_list)
