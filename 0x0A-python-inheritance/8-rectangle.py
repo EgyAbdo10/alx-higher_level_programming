275841198202bb4a81c14925b217efcab52155b2
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-08 15:20:27 -0800

    added or modified a file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
-"""define a rectangle class"""
+"""define a geometry class"""
+
+
+class BaseGeometry:
+    """an empty class BaseGeometry"""
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate an integer and its value"""
+        if (type(value) != int):
+            raise TypeError("{} must be an integer".format(name))
+        if (value <= 0):
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
