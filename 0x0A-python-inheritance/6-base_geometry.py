81b8e8728dd84720ca2475291dacacce8a2bf832
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-08 12:21:41 -0800

    added or modified a file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""define a geometry class"""
+
+
+class BaseGeometry:
+    """an empty class BaseGeometry"""
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
