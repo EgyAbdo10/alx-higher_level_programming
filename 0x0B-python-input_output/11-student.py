006f7405cecdcc620e5e81f0562f51d61af0cd51
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-07 16:24:44 -0800

    added or modified a file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""json representation of objects"""
+
+
+class Student:
+    """create a student base on first, last name and age"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """get a dict from object attributes"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            attr_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    attr_dict[attr] = getattr(self, attr)
+            return attr_dict
+
+    def reload_from_json(self, json):
+        """change object dict values"""
+        for attr, val in json.items():
+            if hasattr(self, attr):
+                setattr(self, attr, val)
