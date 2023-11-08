55abae455c51666e6fdb2c9b5d534e0b0f6f1fa0
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-07 17:14:23 -0800

    added or modified a file

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Create Pascal's Triangle"""
+"""create the pascal triangle"""
+
 
 def my_factorial(n):
+    """get the factorial of a number"""
     if n == 0:
         return 1
     result = 1
@@ -9,21 +11,30 @@ def my_factorial(n):
         result *= i
     return result
 
+
+
 def comb_list(n):
+    """get a list of all the numbers of combinations
+    nCm from m = 0 to m = n"""
     if n < 0:
         return []
     m = n
     comb_list = []
-    for m in range(n + 1):
-        val = my_factorial(n) // (my_factorial(n - m) * my_factorial(m))
+    while (m >= 0):
+        val = int((my_factorial(n) / my_factorial(n - m)) / my_factorial(m))
         comb_list.append(val)
+        m -= 1
     return comb_list
 
+
 def pascal_triangle(n):
+    """draw pascal triange of height n lists"""
     if n <= 0:
         return []
-    pascal_2d_list = []
-    for i in range(n):
-        pascal_2d_list.append(comb_list(i))
-    return pascal_2d_list
-
+    else:
+        pascal_2d_list = []
+        i = 0
+        while (i < n):
+            pascal_2d_list.append(comb_list(i))
+            i += 1
+        return pascal_2d_list
