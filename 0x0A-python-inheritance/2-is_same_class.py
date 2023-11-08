2cb1d7601246e03bddc95f1b82ca9829cd0b29de
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-08 11:53:05 -0800

    added or modified a file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""check if an object is instance of a given class"""
+
+
+def is_same_class(obj, a_class):
+    """check if an object is instance of a given class
+    return true if yes and false otherwise"""
+    return isinstance(obj, a_class)
