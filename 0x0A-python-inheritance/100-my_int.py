db991a026abc5c379491b95cc90983ba16b3e9b0
EgyAbdo10 <egyabdo490@gmail.com>
2023-11-08 16:21:24 -0800

    added or modified a file

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""define a class MyInt to revers the class Int"""
+
+
+class MyInt(int):
+    def __eq__(self, value):
+        """reverse the equal operator"""
+        return super().__ne__(value)
+    
+    def __ne__(self, value):
+        """reverse the not equal operator"""
+        return super().__eq__(value)
