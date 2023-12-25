b0948ededb0360110af9365653045064ea6187ee
EgyAbdo10 <egyabdo490@gmail.com>
2023-12-25 06:12:31 +0200

    added or modified a file

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- top cities in temperatures
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
+WHERE month = 8 OR month = 7
 GROUP BY city
 ORDER BY AVG(value) DESC
-WHERE month = 8 OR month = 7
