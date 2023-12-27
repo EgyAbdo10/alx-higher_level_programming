2abc3e4773852a95e452746ad4e80f76c2217876
EgyAbdo10 <egyabdo490@gmail.com>
2023-12-27 07:35:01 +0200

    added or modified a file

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,7 @@ SELECT g.name
 FROM tv_genres AS g
 LEFT JOIN tv_show_genres AS sg
 ON sg.genre_id = g.id
-LEFT JOIN tv_shows AS s
+RIGHT JOIN tv_shows AS s
 ON s.id = sg.show_id
 WHERE s.title = "Dexter"
 ORDER BY g.name ASC
