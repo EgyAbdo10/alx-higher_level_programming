b760491817a1a2ca00f2314ffa84fa9010d7a442
EgyAbdo10 <egyabdo490@gmail.com>
2023-12-27 07:24:52 +0200

    added or modified a file

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- count genres
+SELECT g.name AS genre, count(g.name) AS number_of_shows
+FROM tv_genres AS g
+LEFT JOIN tv_show_genres AS sg
+ON sg.genre_id = g.id
+LEFT JOIN tv_shows AS s
+ON s.id = sg.show_id
+GROUP BY g.name
