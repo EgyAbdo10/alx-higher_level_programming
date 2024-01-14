63573a9e1ee907e89e25096639a091c6963f6c64
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-14 21:38:57 +0200

    not important

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 function secondBiggestNum (arr) {
+  if (arr.length < 2) {
+    return (0);
+  }
   arr = arr.map(item => Number(item));
   const arrMax = Math.max(...arr);
   const arrMaxIdx = arr.indexOf(arrMax);
