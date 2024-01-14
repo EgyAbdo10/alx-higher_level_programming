88aac51777e6c7be25798a0ca708520b01026642
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-14 20:50:34 +0200

    not important

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,6 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-//   let i = 0;
   for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
   }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,5 +2,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-    
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('X'.repeat(process.argv[2]));
+  }
 }
