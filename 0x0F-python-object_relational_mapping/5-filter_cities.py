5b6514e9fbc7d91e775c5f35ac759304c1695d93
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-26 03:14:54 +0200

    alx task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+this modules filters all the cities with their states
+"""
+
+from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy.orm import sessionmaker
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db_conn = MySQLdb.connect(host="localhost", user=argv[1],
+                              passwd=argv[2], db=argv[3], port=3306)
+    cur = db_conn.cursor()
+    # engine = create_engine("mysql+mysqldb://", creator=lambda: db_conn)
+    # Session = sessionmaker(bind=engine)
+    # session = Session()
+    # metadata = MetaData()
+    # States = Table("states", metadata, autoload_with=engine)
+    # Citeies = Table("cities", metadata, autoload_with=engine)
+    state_name = argv[4]
+    cur.execute(f"""SELECT c.id, c.name, s.name FROM cities as c
+                    join states as s
+                    on s.id = c.state_id
+                    where s.name = '{state_name}'""")
+    # records = session.query(Citeies.c.name.label("city_name"),
+    #                         Citeies.c.id.label("city_id"),
+    #                         States).filter(Citeies.c.state_id == States.c.id)
+    records = cur.fetchall()
+    for rec in records:
+        # print("({}, '{}', '{}')".format(
+        # rec.city_id, rec.city_name, rec.name))
+        print(rec)
+    # session.close()
