35c70da52c3c76e5051b52a9b96de4e0f3795040
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-26 03:09:30 +0200

    alx task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,18 +12,22 @@ from sys import argv
 if __name__ == "__main__":
     db_conn = MySQLdb.connect(host="localhost", user=argv[1],
                               passwd=argv[2], db=argv[3], port=3306)
-
-    engine = create_engine("mysql+mysqldb://", creator=lambda: db_conn)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    metadata = MetaData()
-    States = Table("states", metadata, autoload_with=engine)
-    Citeies = Table("cities", metadata, autoload_with=engine)
-
-    records = session.query(Citeies.c.name.label("city_name"),
-                            Citeies.c.id.label("city_id"),
-                            States).filter(Citeies.c.state_id == States.c.id)
-
+    cur = db_conn.cursor()
+    # engine = create_engine("mysql+mysqldb://", creator=lambda: db_conn)
+    # Session = sessionmaker(bind=engine)
+    # session = Session()
+    # metadata = MetaData()
+    # States = Table("states", metadata, autoload_with=engine)
+    # Citeies = Table("cities", metadata, autoload_with=engine)
+    cur.execute("""SELECT c.id, c.name, s.name FROM cities as c
+                    join states as s
+                    on s.id = c.state_id""")
+    # records = session.query(Citeies.c.name.label("city_name"),
+    #                         Citeies.c.id.label("city_id"),
+    #                         States).filter(Citeies.c.state_id == States.c.id)
+    records = cur.fetchall()
     for rec in records:
-        print("({}, '{}', '{}')".format(rec.city_id, rec.city_name, rec.name))
-    session.close()
+        # print("({}, '{}', '{}')".format(
+        # rec.city_id, rec.city_name, rec.name))
+        print(rec)
+    # session.close()
