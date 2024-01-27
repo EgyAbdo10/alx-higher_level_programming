ca3bca8c79f269dffc78b61ca3ee3a4fd75465da
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-27 04:25:44 +0200

    alx task

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
+"""Lists all State objects from the database hbtn_0e_6_usa.
+Usage: ./7-model_state_fetch_all.py <mysql username> /
+                                    <mysql password> /
+                                    <database name>
+"""
 
-"""this is a module to retrieve data from States table in a db"""
-
-from model_state import Base, State
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine(
-        f"mysql+mysqlconnector://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}",
-        pool_pre_ping=True
-        )
-
-    Base.metadata.create_all(bind=engine)
-
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    records_ = session.query(State).order_by(State.id.asc())
-
-    for rec in records_:
-        print(f"{rec.id}: {rec.name}")
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
