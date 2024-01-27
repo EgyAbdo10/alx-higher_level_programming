6e4883833010dbd0c807d9ee9a6b0dae6c5283bd
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-27 04:18:24 +0200

    alx task

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""this is a module to retrieve data from States table in a db"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f"mysql+mysqlconnector://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+        )
+
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    records_ = session.query(State).order_by(State.id.asc()).all()
+
+    for rec in records_:
+        print(f"{rec.id}: {rec.name}")
