3f42fe1c88a03f79d85b82061a3aea59293d5767
EgyAbdo10 <egyabdo490@gmail.com>
2024-01-27 03:34:21 +0200

    alx task

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""this is a module to retrieve data from States table in a db"""
+
+from sqlalchemy import Integer, Null, String, create_engine, Column
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+Base = declarative_base()
+
+
+class States(Base):
+    """this class creates a States Table in a passed db"""
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, autoincrement=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+
+engine = create_engine(
+    f"mysql+mysqlconnector://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    )
+
+Base.metadata.create_all(bind=engine)
