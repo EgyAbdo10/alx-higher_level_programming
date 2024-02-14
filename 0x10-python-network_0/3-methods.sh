9da665b4f7cabbddc5d2d96671c5ceb827b9ec12
EgyAbdo10 <egyabdo490@gmail.com>
2024-02-14 14:59:01 +0200

    not important

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # show all available methods allowed by a server
-curl -sX OPTIONS "$1" | grep "Allow: " | grep -oE "\b [a-zA-Z]+\b"
+curl -sX OPTIONS "$1" | grep "Allow: " | cut -d" " -f2-
