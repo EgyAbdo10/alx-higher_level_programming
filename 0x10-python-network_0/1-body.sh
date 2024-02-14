fd9310a37780cad3f8ae54c377d7da42cd2506c0
EgyAbdo10 <egyabdo490@gmail.com>
2024-02-14 14:29:41 +0200

    not important

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # get the body of a 200 response
-if [ $(curl -si "$1" | head -n 1 | grep -oE " [0-9]+") -eq " 200" ]; then curl -s --GET "$1"; else curl -s "$1"; fi
+if [ $(curl -si "$1" | head -n 1 | grep -oE " [0-9]+") -eq " 200" ]; then curl -s --GET "$1"; else curl -si "$1"; fi
