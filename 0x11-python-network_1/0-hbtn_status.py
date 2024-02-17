4bf14154327e3fe900ca0340b38466142222c4f7
EgyAbdo10 <egyabdo490@gmail.com>
2024-02-17 11:49:41 +0200

    not important

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this module fetches https://alx-intranet.hbtn.io/status resources"""
+
+
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        charset = response.headers.get_content_charset()
+        content_b = response.read()
+        content = content_b.decode("utf-8")
+        print(f"    - type: {type(content_b)}")
+        print(f"    - content: {content_b}")
+        print(f"    - utf8 content: {content}")
