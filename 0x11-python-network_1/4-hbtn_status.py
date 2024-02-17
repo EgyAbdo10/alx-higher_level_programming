2ef2268d16e33aa28efced9438d4a568105f5111
EgyAbdo10 <egyabdo490@gmail.com>
2024-02-17 14:18:04 +0200

    not important

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,10 @@ Write a Python script that fetches https://alx-intranet.hbtn.io/status
 import requests
 
 if __name__ == "__main__":
-    print()
+    # Body response:$
+    # - type: <class 'str'>$
+    # - content: OK$
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
