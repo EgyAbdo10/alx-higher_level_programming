4fb87460c9a925ddb4cb4b4ac5350781dc6e2578
EgyAbdo10 <egyabdo490@gmail.com>
2024-02-17 14:34:55 +0200

    not important

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,3 +12,5 @@ if __name__ == "__main__":
     response = requests.get(argv[1])
     if response.status_code > 400:
         print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
