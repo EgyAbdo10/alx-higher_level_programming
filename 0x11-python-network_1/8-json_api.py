0982380d57b6059803c1291bc7b72861c1b59091
EgyAbdo10 <egyabdo490@gmail.com>
2024-02-17 14:45:38 +0200

    not important

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python3
 """
-
+Write a Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data = {"q": argv[1]}
+    if len(argv) > 2:
+        data = {"q": ""}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        res_dict = response.json()
+        if res_dict == {}:
+            print("No result")
+        else:
+            print(f"[{res_dict.get("id")}] {res_dict.get("name")}")
+    except TypeError:
+        print("Not a valid JSON")
