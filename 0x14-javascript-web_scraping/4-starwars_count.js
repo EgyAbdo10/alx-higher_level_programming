cbfdf3062bc846b47e8e082085f05815e53c0192
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-02 06:51:03 +0300

    not important

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+//     "characters": [
+// "https://swapi-api.alx-tools.com/api/people/1/",
+function getMoviesChar (callback) {
+  let numOccerances = 0;
+  const url = 'https://swapi-api.alx-tools.com/api/films/1';
+  const charRecord = 'https://swapi-api.alx-tools.com/api/people/18/';
+  let id = 1;
+  function requestNext (url) {
+    request(url, (err, response, body) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (response.statusCode === 404) {
+        callback(numOccerances);
+      } else {
+        const data = JSON.parse(body);
+        if (data.characters.includes(charRecord)) {
+          numOccerances += 1;
+        }
+        id += 1;
+        url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+        requestNext(url);
+      }
+    });
+  }
+  requestNext(url);
+}
+
+getMoviesChar((numOccerances) => {
+  console.log(numOccerances);
+});
