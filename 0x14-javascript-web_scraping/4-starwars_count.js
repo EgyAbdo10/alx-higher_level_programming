936985d03cfe95a166e14481c81492a22103e932
EgyAbdo10 <egyabdo490@gmail.com>
2024-08-02 07:14:57 +0300

    not important

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request');
 //     "characters": [
 // "https://swapi-api.alx-tools.com/api/people/1/",
 function getMoviesChar () {
-  const url = 'https://swapi-api.alx-tools.com/api/films/';
+  const url = process.argv[2];
   const charRecord = 'https://swapi-api.alx-tools.com/api/people/18/';
   let numOcc = 0;
   request(url, (err, response, body) => {
